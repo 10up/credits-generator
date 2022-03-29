06f51b00c0ab6a6ea5c1c2a5fc5857d1faea9ae4
Tung Du <dinhtungdu@gmail.com>
2022-03-29 14:10:20 +0700

    fix: query reviews and review comments, closes #5

--- cli.js
+++ cli.js
@@ -113,6 +113,51 @@ async function run() {
 		return comments.concat(newComments);
 	}
 
+	async function getReviews(pulls, reviews = []) {
+		const repo = await getRepoInfo();
+		const current = pulls.shift();
+		const newReviews = await octokit.request(
+			"GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews",
+			{
+				owner: repo.owner,
+				repo: repo.name,
+				pull_number: current.number,
+			}
+		);
+
+		if( pulls.length == 0 ) {
+			return [...reviews, ...newReviews.data];
+		}
+
+		return await getReviews(pulls, [...reviews, ...newReviews.data]);
+	}
+
+	async function getReviewComments(comments = [], page = 1) {
+		const repo = await getRepoInfo();
+		const batch = await octokit.request(
+			"GET /repos/{owner}/{repo}/pulls/comments",
+			{
+				owner: repo.owner,
+				repo: repo.name,
+				per_page: 100,
+				page: page,
+				...(since ? { since } : {}),
+			}
+		);
+
+		if (batch.status !== 200) {
+			return comments;
+		}
+
+		const newComments = batch.data;
+
+		if (newComments.length > 100) {
+			return await getReviewComments(comments.concat(newComments), page + 1);
+		}
+
+		return comments.concat(newComments);
+	}
+
 	async function getUserFullName(user) {
 		const { data } = await octokit.request("GET /users/{user}", {
 			user: user,
@@ -122,16 +167,29 @@ async function run() {
 	}
 
 	const issues = await getIssues();
+	const pulls = issues.filter(issue => issue.pull_request);
+	const reviews = await getReviews(pulls);
+
 	const comments = await getIssueComments();
+	const reviewComments = await getReviewComments();
 
 	const issueUrls = issues.map((issue) => issue.url);
-	const reporters = issues.map((issue) => issue.user.login);
+	const pullUrls = pulls.map(pull => pull.pull_request.url)
 
+	const authors = issues.map((issue) => issue.user.login);
+	const reviewers = reviews.map((review) => review.user.login);
+
+	/**
+	 * Filter only comments belong to closed issues and pull requests.
+	 */
 	const commenters = comments
 		.filter((comment) => issueUrls.includes(comment.issue_url))
 		.map((comment) => comment.user.login);
+	const reviewerCommenters = reviewComments
+		.filter((comment) => pullUrls.includes(comment.pull_request_url))
+		.map((comment) => comment.user.login);
 
-	let contributors = [...new Set(reporters.concat(commenters))];
+	let contributors = [...new Set([...authors, ...reviewers, ...commenters, ...reviewerCommenters])];
 
 	const excludedContributors = cli.flags.exclude.split(",");
 
